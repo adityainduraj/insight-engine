50ce389326fbf0559bfe4777b62c13293cd9feda
adityainduraj <85331139+adityainduraj@users.noreply.github.com>
2024-08-08 19:47:46 +0530

    re-initializing venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adityainduraj/Documents/projects/Insight Engine/venv"
+set -gx VIRTUAL_ENV "/home/adityainduraj/Documents/projects/insight engine/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
